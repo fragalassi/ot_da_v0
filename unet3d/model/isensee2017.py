96f0fd7ac125a062e2da573fe93f3cc4f981079d
Antoine Ackaouy <antoine.ackaouy@gmail.com>
2019-02-15 14:06:30 +0100

    Forgot isensee2017.py

--- unet3d/model/isensee2017.py
+++ unet3d/model/isensee2017.py
@@ -12,7 +12,7 @@ create_convolution_block = partial(create_convolution_block, activation=LeakyReL
 
 def isensee2017_model(input_shape=(2, 200, 200, 200), n_base_filters=16, depth=5, dropout_rate=0.3,
                       n_segmentation_levels=3, n_labels=1, optimizer=Adam, initial_learning_rate=5e-4,
-                      loss_function=generalized_dice_loss, activation_name="sigmoid"):
+                      loss_function=weighted_dice_coefficient_loss, activation_name="sigmoid"):
                           
     """
     This function builds a model proposed by Isensee et al. for the BRATS 2017 competition:
@@ -78,6 +78,9 @@ def isensee2017_model(input_shape=(2, 200, 200, 200), n_base_filters=16, depth=5
 
     model = Model(inputs=inputs, outputs=activation_block)
     model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
+    print("Optimizer: ", optimizer)
+    print("Initial LR: ", initial_learning_rate)
+    print("Loss function: ", loss_function)
     return model
 
 
