a947bf1e22f32fea4ff26abe1b68be81985224f2
Antoine Ackaouy <antoine.ackaouy@gmail.com>
2019-02-18 10:57:05 +0100

    Try 3

--- MAIN_igrida.sh
+++ MAIN_igrida.sh
@@ -6,7 +6,17 @@ echo "$SCRIPTDIR"
 
 chmod u+x $SCRIPTDIR/bypass.sh
 
-oarsub -l {"host = 'igrida-abacus4.irisa.fr'"}/nodes=1/gpu_device=1,walltime=1:00:0 "$SCRIPTDIR/bypass.sh"
+oarsub -I -l {"host = 'igrida-abacus4.irisa.fr'"}/nodes=1/gpu_device=1,walltime=1:00:0
+set -xv
+
+. /etc/profile.d/modules.sh
+
+module load cuDNN/v7.0.4
+module load cuda/9.0.176
+
+# Activate the py virtual environnement
+. /udd/aackaouy/myVE/bin/activate
+PYTHONHASHSEED=0 python3 -W ignore $SCRIPTDIR/main.py
 
 #oarsub -t besteffort -t idempotent -p "dedicated='none' or dedicated = 'serpico' " -l {"gpu_model = 'Tesla P100'"}/gpu_device=1,walltime=48:00:0 "$SCRIPTDIR/bypass.sh"
 
--- config.py
+++ config.py
@@ -53,6 +53,8 @@ class Config:
         self.overwrite = True  # If True, will previous files. If False, will use previously written files.
 
         self.data_file = os.path.abspath("Data/generated_data/"+self.data_set+"_data.h5")
+        if not os.path.abspath("Data/generated_data/"):
+            os.makedirs(os.path.abspath("Data/generated_data/"))
 
         self.n_base_filters = 16
         self.depth = int(depth)
