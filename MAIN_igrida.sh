183e4082ac1e48b4db69c97b9ae0be653ed9eac5
Antoine Ackaouy <antoine.ackaouy@gmail.com>
2019-02-18 11:11:49 +0100

    Trying to run from sh

--- MAIN_igrida.sh
+++ MAIN_igrida.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 # SCRIPT dir
-SCRIPTDIR=/udd/aackaouy/OT-DA
-echo "$SCRIPTDIR"
-
-chmod u+x $SCRIPTDIR/bypass.sh
 
 oarsub -I -l {"host = 'igrida-abacus4.irisa.fr'"}/nodes=1/gpu_device=1,walltime=1:00:0
-set -xv
-
-. /etc/profile.d/modules.sh
 
 module load cuDNN/v7.0.4
 module load cuda/9.0.176
 
 # Activate the py virtual environnement
 . /udd/aackaouy/myVE/bin/activate
-PYTHONHASHSEED=0 python3 -W ignore $SCRIPTDIR/main.py
+PYTHONHASHSEED=0 python3 -W ignore /udd/aackaouy/OT-DA/main.py
 
 #oarsub -t besteffort -t idempotent -p "dedicated='none' or dedicated = 'serpico' " -l {"gpu_model = 'Tesla P100'"}/gpu_device=1,walltime=48:00:0 "$SCRIPTDIR/bypass.sh"
 
