0fda1f4045bbeefb37c530fdfe31d7572138cba7
Antoine Ackaouy <antoine.ackaouy@gmail.com>
2019-02-19 10:56:35 +0100

    Test for niseko

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import config
 import sys
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="0" # Enlever pour igrida
 import tensorflow as tf
 import pandas as pd
 import numpy as np
