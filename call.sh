981900baa3f925382591b3e10ce527c5bc2c721a
Antoine Ackaouy <antoine.ackaouy@gmail.com>
2019-06-24 12:11:05 +0200

    Added an intensity ceil option

--- call.sh
+++ call.sh
@@ -1,3 +1,3 @@
-python3 -u main.py -source '01' -target '07' -intensity_ceil -0.1 -alpha 0.01 -beta 0.001 -jdot 'False' -epochs 2000 -load_model 'False' -rev 0 > Logs/output_0.log
-python3 -u main.py -source '07' -target '08' -intensity_ceil -0.1 -alpha 0.01 -beta 0.001 -jdot 'False' -epochs 2000 -load_model 'False' -rev 1 > Logs/output_1.log
-python3 -u main.py -source '08' -target '01' -intensity_ceil -0.1 -alpha 0.01 -beta 0.001 -jdot 'False' -epochs 2000 -load_model 'False' -rev 2 > Logs/output_2.log
+python3 -u main.py -source '01' -target '07' -split_list '(([0, 2, 1, 4], [3]), ([0, 3, 2, 4], [1]))' -intensity_ceil -0.40 -alpha 40 -beta 10 -jdot 'False' -epochs 60 -load_model 'True' -rev 0 > Logs/output_0.log
+python3 -u main.py -source '01' -target '08' -split_list '(([0, 2, 1, 4], [3]), ([0, 1, 2, 4], [3]))' -intensity_ceil -0.40 -alpha 40 -beta 10 -jdot 'False' -epochs 60 -load_model 'True' -rev 0 > Logs/output_0.log
+python3 -u main.py -source '08' -target '01' -split_list '(([0, 2, 1, 4], [3]), ([0, 1, 2, 4], [3]))' -intensity_ceil -0.40 -alpha 40 -beta 10 -jdot 'False' -epochs 60 -load_model 'True' -rev 0 > Logs/output_0.log
